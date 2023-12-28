0fd415da0b4e46c5fb3a828c9d8bd2fc972e4ad4
Aloïs R. <alois.rautureau@ens-rennes.fr>
2023-12-28 20:57:20 +0100

    Added bat alias

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -9,6 +9,9 @@ alias ls exa
 alias tree "exa --tree"
 alias hx helix
 alias sudo doas
+alias cat bat
+
+set -Ux EDITOR helix
 
 function fish_prompt
     printf '%s%s%s > ' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
